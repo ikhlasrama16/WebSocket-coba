7cbc35df66c0003e138d9897ec7bbda414eb8d85
ikhlasrama16 <ikhlasrama16@gmail.com>
2024-10-21 17:04:08 +0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const { Socket } = require('dgram');
+const ws = require('ws');
+const server = new ws.Server({port: '3000'});
+
+server.on('connection', socket => {
+    socket.on('message', message=>{
+        console.log(message)
+        socket.send(`${message}`)
+    });
+});
